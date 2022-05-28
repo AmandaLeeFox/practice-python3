8bf96f7ed849f87c52f7f1b0aa71ad5d45d814e2
AmandaLee <104425046+AmandaLeeFox@users.noreply.github.com>
2022-05-28 17:27:48 +0800

    Update function Return.py

--- function Return.py
+++ function Return.py
@@ -1,1 +1,11 @@
+#函数返回值
+#可使用return语句将值返回到调用函数的代码行
 
+#返回简单值
+def get_formatted_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+me = get_formatted_name('Amanda','Lee')
+print(me)
+
+#可选的实参
