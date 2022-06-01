6e9d484b701d84b94e64afc0f20868af3eb84406
AmandaLee <104425046+AmandaLeeFox@users.noreply.github.com>
2022-06-01 10:10:54 +0800

    Update function Return.py

--- function Return.py
+++ function Return.py
@@ -8,4 +8,28 @@ def get_formatted_name(first_name, last_name):
 me = get_formatted_name('Amanda','Lee')
 print(me)
 
-#可选的实参
+
+# 实参变为可选的，给某个值设定一个空字符，未提供时不使用
+def get_name(first_name, last_name, middle_name = ''):
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' +last_name
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+me = get_name('Amanda', 'Lee')
+me2 = get_name('Amanda', 'Lee', 'mid') #按照实参顺序
+print(me)
+print(me2)
+
+
+#返回字典，【定义函数，返回字典，包含一个人的信息，并加年龄信息未默认空值参数】
+def get_person(first_name, last_name, age = ''):
+    person = {
+        'first_name' : first_name,
+        'last_name' : last_name
+    }
+    if age:
+        person['age'] = age #如果有age，则加入字典
+    return person
+me = get_person('Amanda', 'Lee', 27) #或age = 27
+print(me)
